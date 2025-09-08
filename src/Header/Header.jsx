97c31d08e4e6015e1c8c2ad48b177c77c8e246a6
Konkamon Fungsuk <konkamon@sbo-brand.com>
2025-09-08 17:29:46 +0700

    main

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import LogononText from "../assets/LogononText.png";
+import { IoPersonCircleOutline } from "react-icons/io5";
+import { MdPersonAddAlt1, MdPersonSearch } from "react-icons/md";
 
 export default function Header() {
   return (
     <>
-    <div>Header</div>
+      <div className="flex justify-center items-center">
+        <a href="/">Home</a>
+        <img src={LogononText} alt="" className="w-[10%]" />
+        <a href="/">Services</a>
+        <a href="/">Visitor Guides</a>
+        <a href="/">Be Healthy</a>
+        <a href="/">About Us</a>
+        <div>
+          <a href="/">
+            <IoPersonCircleOutline size={30} />
+          </a>
+          <div className="flex items-center">
+            Sign-In
+            <MdPersonSearch />
+          </div>
+          <div className="flex items-center">
+            Sign-Up
+            <MdPersonAddAlt1 />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
--- src/Router.jsx
+++ src/Router.jsx
@@ -2,19 +2,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainComponent from "./component/MainComponent";
 
 const route = createBrowserRouter([
-    {
-        path : "/",
-        element: <MainComponent/>,
-        children:[{
+  {
+    path: "/",
+    element: <MainComponent />,
+    children: [
+        { path: "/", element: <MainComponent /> },
+       
+    ],
+  },
+]);
 
-        }]
-    }
-])
-
-export default function Router(){
-    return (
-        <div>
-            <RouterProvider route={route}/>
-        </div>
-    )
+export default function Router() {
+  return (
+    <div>
+      <RouterProvider route={route} />
+    </div>
+  );
 }
