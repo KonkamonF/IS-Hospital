e7944b973a701dfd2acf2dc8167dc24e88cea5c0
Konkamon Fungsuk <konkamon@sbo-brand.com>
2025-09-16 14:07:42 +0700

    path

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -118,21 +118,21 @@ export default function Header() {
             <NavDropdown
               label="Services"
               items={[
-                { label: "1", href: "/Services" },
+                { label: "1", href: "Services" },
                 { label: "2", href: "/Services/2" },
               ]}
             />
             <NavDropdown
               label="Visitor Guides"
               items={[
-                { label: "1", href: "/Visitor" },
+                { label: "1", href: "Visitor" },
                 { label: "2", href: "/Visitor/2" },
               ]}
             />
             <NavDropdown
               label="Be Healthy"
               items={[
-                { label: "1", href: "/Healthy" },
+                { label: "1", href: "Healthy" },
                 { label: "2", href: "/Healthy/2" },
               ]}
             />
@@ -213,19 +213,19 @@ export default function Header() {
               Home
             </a>
             <a
-              href="/Services"
+              href="Services"
               className="block px-4 py-2 hover:rounded-xl hover:bg-[#42C2FF] hover:text-white"
             >
               Services
             </a>
             <a
-              href="/Visitor"
+              href="Visitor"
               className="block px-4 py-2 hover:rounded-xl hover:bg-[#42C2FF] hover:text-white"
             >
               Visitor Guides
             </a>
             <a
-              href="/Healthy"
+              href="Healthy"
               className="block px-4 py-2 hover:rounded-xl hover:bg-[#42C2FF] hover:text-white"
             >
               Be Healthy
